72c5e993ea27c6bc12006d5ed75b44636abc03d7
Raphael Lima <shd.contato@gmail.com>
2016-01-06 16:22:57 -0200

    Phantom JS

--- config/protractor.js
+++ config/protractor.js
@@ -5,8 +5,8 @@ exports.config = {
 	onPrepare: function(){
 		browser.driver.get('http://localhost:3000/login');
 		browser.driver.findElement(by.id('entrar')).click();
-		browser.driver.findElement(by.id('login_field')).sendKeys('raphael.aolima8@gmail.com');
-		browser.driver.findElement(by.id('password')).sendKeys('arthur123');
+		browser.driver.findElement(by.id('login_field')).sendKeys();
+		browser.driver.findElement(by.id('password')).sendKeys();
 		browser.driver.findElement(by.name('commit')).click();
 		//browser.driver.findElement(by.name('authorize')).click();
 	}
