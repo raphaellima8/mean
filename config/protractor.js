e0ce0705320101b54e0ec0e57103a6a2bf4fc787
Raphael Lima <shd.contato@gmail.com>
2016-01-07 11:49:45 -0200

    Teste

--- config/protractor.js
+++ config/protractor.js
@@ -5,8 +5,8 @@ exports.config = {
 	onPrepare: function(){
 		browser.driver.get('http://localhost:3000/login');
 		browser.driver.findElement(by.id('entrar')).click();
-		browser.driver.findElement(by.id('login_field')).sendKeys();
-		browser.driver.findElement(by.id('password')).sendKeys();
+		browser.driver.findElement(by.id('login_field')).sendKeys('raphael.aolima8@gmail.com');
+		browser.driver.findElement(by.id('password')).sendKeys('1234567a');
 		browser.driver.findElement(by.name('commit')).click();
 		browser.driver.findElement(by.name('authorize')).click();
 	}
