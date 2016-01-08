87a54359704bb30e90e79acfb75a36f2c205ff11
Raphael Lima <shd.contato@gmail.com>
2016-01-08 11:22:27 -0200

    protractor.js

--- config/protractor.js
+++ config/protractor.js
@@ -4,7 +4,7 @@ exports.config = {
 	sauceKey: config.sauceKey,
 	capabilities : {
 		'name': config.sauceTestName,
-		'browser-name': chrome,
+		'browser-name': 'chrome',
 		'tunnel-identifier': config.travisJobNumber,
 		'build': config.travisBuild
 	},
