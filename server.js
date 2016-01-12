19f7a883bb31d19135e7f44a64419a2694138f01
Raphael Lima <shd.contato@gmail.com>
2016-01-12 14:34:25 -0200

    Deploy

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ require('./config/passport')();
 require('./config/database.js')('mongodb://localhost/mean');
 var config = require('./config/config')();
 require('./config/database')(config.db);
-http.createServer(app).listen(config.env, config.port, config.address, function(){
+http.createServer(app).listen(config.port, config.address, function(){
   console.log('Express HTTP Server ' + config.address + ' (' + config.env + ') escutando na porta ' + config.port);
 });
