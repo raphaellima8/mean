4fc112b8cf38ef12bef15d113d0a6688f760aa29
Raphael Lima <shd.contato@gmail.com>
2016-01-12 12:07:16 -0200

    Deploy

--- config/express.js
+++ config/express.js
@@ -10,7 +10,7 @@ var helmet = require('helmet');
 module.exports = function(){
   var app = express();
 
-  app.set('port', 8080);
+  app.set('port', 80);
 
   //Setando a template engine
   app.set('view engine', 'ejs');
