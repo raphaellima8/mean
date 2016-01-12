ffacc9f982cdcd0e05affe09756b7430bc653be0
Raphael Lima <shd.contato@gmail.com>
2016-01-12 16:27:50 -0200

    deploy

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
 var findOrCreate = require('mongoose-findorcreate');
 var mongoose = require('mongoose');
-var githubCallback = 'http://' + config.domain + ':' + config.port + '/auth/github/callback';
+var githubCallback = 'http://' + config.domain + '/auth/github/callback';
 
 module.exports = function(){
 
