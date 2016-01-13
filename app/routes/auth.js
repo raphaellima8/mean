571e20098c4bf36a34179d92da7fd0b1c622d825
Raphael Lima <shd.contato@gmail.com>
2016-01-13 13:05:31 -0200

    deploy

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,7 +3,7 @@ var passport = require('passport');
 module.exports = function(app){
 
 	app.get('/auth/github', passport.authenticate('github'));
-	app.get('/', passport.authenticate('github', {
+	app.get('/callback', passport.authenticate('github', {
 		successRedirect: '/'
 	}));
 
