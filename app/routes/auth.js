fa0819481859b1261cd7e72d7ce841628fa7be66
Raphael Lima <shd.contato@gmail.com>
2016-01-14 16:24:50 -0200

    Teste

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -21,7 +21,8 @@ module.exports = function(app){
 		}
 	});
 
-	app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.me']}));
+	app.get('/auth/google/', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.me',
+		'https://www.googleapis.com/auth/plus.login']}));
 	app.get('/auth/google/callback/', passport.authenticate('google', 
 		{
 			successRedirect: '/'
