f229ef093a6e75e0395a99fc230f86e0fd14399e
Raphael Lima <shd.contato@gmail.com>
2016-01-14 13:56:28 -0200

    Teste

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,8 @@
 module.exports = {
 	env: 'production',
 	db: process.env.OPENSHIFT_MONGODB_DB_URL + '/mean',
-	clientID: process.env.CLIENT_ID,
-	clientSecret: process.env.CLIENT_SECRET,
+	clientID: process.env.CLIENT_ID || process.env.CLIENT_ID_GOOGLE,
+	clientSecret: process.env.CLIENT_SECRET || process.env.CLIENT_SECRET_GOOGLE,
 	port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	address: process.env.OPENSHIFT_NODEJS_IP,
 	domain: process.env.OPENSHIFT_APP_DNS
